385035f03771a1944874902780f3855712dd1279
chocoby <chocoby@gmail.com>
2011-03-31 17:03:34 +0900

    セットアップスクリプト修正

--- setup.sh
+++ setup.sh
@@ -2,8 +2,15 @@
 cd $(dirname $0)
 for dotfile in .?*
 do
-    if [ $dotfile != '..' ] && [ $dotfile != '.git' ] && [ $dotfile != 'README' ]
+    if [ $dotfile != '..' ] && [ $dotfile != '.git' ]
     then
         ln -Fis "$PWD/$dotfile" $HOME
     fi
 done
+
+mkdir -p $HOME/.zsh
+mkdir -p $HOME/.vim_swap
+mkdir -p $HOME/.vim_backup
+
+git submodule init
+git submodule update
