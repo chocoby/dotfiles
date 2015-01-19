1caa187d1de3cff40488deb5833bc98b5334f7e5
chocoby <chocoby@gmail.com>
2015-01-19 11:51:10 +0900

    Ignore copy .gitmodules and .gitignore

--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@
 cd $(dirname $0)
 for dotfile in .?*
 do
-    if [ $dotfile != '..' ] && [ $dotfile != '.git' ]
+    if [ $dotfile != '..' ] && [ $dotfile != '.git' ] && [ $dotfile != '.gitignore' ] && [ $dotfile != '.gitmodules' ]
     then
         ln -Fis "$PWD/$dotfile" $HOME
     fi
