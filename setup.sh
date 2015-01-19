d499c65a96902d8c37e54d0a4bdb46892cdbad70
chocoby <chocoby@gmail.com>
2015-01-19 12:42:38 +0900

    Configure git color, gitignore when run setup

--- setup.sh
+++ setup.sh
@@ -11,8 +11,13 @@ done
 git submodule init
 git submodule update
 
+# Vim
 mkdir -p $HOME/.vim_swap
 mkdir -p $HOME/.vim_backup
 
 mkdir -p $HOME/.vim/bundle
 git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
+
+# Git
+git config --global core.excludesfile ~/.gitignore_global
+git config --global color.ui auto
