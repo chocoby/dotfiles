4277f19f03629014f9ec37cb4b04562f5e0239e2
Kenta Okamoto <chocoby@gmail.com>
2016-08-11 18:21:49 +0900

    Vim: Support update vim plugins on macOS

--- Makefile
+++ Makefile
@@ -3,12 +3,17 @@ ifeq ($(UNAME), Linux)
 	include Makefile.Linux
 endif
 
+ifeq ($(UNAME), Darwin)
+	include Makefile.macOS
+endif
+
 all: install
 
 deploy:
 	bash scripts/setup_dotfiles.sh
 
 init:
+	bash scripts/setup_vim.sh
 
 update:
 	git pull origin master
--- scripts/setup_vim.sh
+++ scripts/setup_vim.sh
@@ -2,12 +2,37 @@
 
 cd $(dirname "$0")
 
-export VIM_REPO=https://github.com/vim/vim.git
-export VIM_SRC_PATH=../tmp/vim_src
-export NUMCPUS=`grep -c '^processor' /proc/cpuinfo`
-export PYTHON_CONFIG_DIR=/usr/lib/python2.7/config-x86_64-linux-gnu
+if [ "$(uname)" == 'Darwin' ]; then
+    VIM=/Applications/MacVim.app/Contents/MacOS/Vim
+elif [ "$(expr substr $(uname -s) 1 5)" == 'Linux' ]; then
+    VIM=$HOME/local/bin/vim
 
-mkdir ../tmp
+    export VIM_REPO=https://github.com/vim/vim.git
+    export VIM_SRC_PATH=../tmp/vim_src
+    export NUMCPUS=`grep -c '^processor' /proc/cpuinfo`
+    export PYTHON_CONFIG_DIR=/usr/lib/python2.7/config-x86_64-linux-gnu
+
+    mkdir ../tmp
+
+    git clone $VIM_REPO $VIM_SRC_PATH
+    cd $VIM_SRC_PATH
+
+    git fetch origin
+    git reset --hard origin/master
+
+    make clean
+
+    ./configure \
+        --with-features=huge \
+        --enable-multibyte \
+        --enable-fontset \
+        --enable-pythoninterp \
+        --with-python-config-dir=$PYTHON_CONFIG_DIR \
+        --prefix=$HOME/local
+
+    make -j $NUMCPUS
+    make install
+fi
 
 mkdir -p $HOME/.vim_swap
 mkdir -p $HOME/.vim_backup
@@ -15,24 +40,5 @@ mkdir -p $HOME/.vim_backup
 mkdir -p $HOME/.vim/plugged
 curl -fLo $HOME/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
-git clone $VIM_REPO $VIM_SRC_PATH
-cd $VIM_SRC_PATH
-
-git fetch origin
-git reset --hard origin/master
-
-make clean
-
-./configure \
-  --with-features=huge \
-  --enable-multibyte \
-  --enable-fontset \
-  --enable-pythoninterp \
-  --with-python-config-dir=$PYTHON_CONFIG_DIR \
-  --prefix=$HOME/local
-
-make -j $NUMCPUS
-make install
-
-$HOME/local/bin/vim --version
-$HOME/local/bin/vim +PlugUpgrade +PlugUpdate +PlugClean! +qall > /dev/null
+$VIM --version
+$VIM +PlugUpgrade +PlugUpdate +PlugClean! +qall > /dev/null
