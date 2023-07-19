285605fbb5663d43b67b98de94d25bd4932f2a5d
Kenta Okamoto <chocoby@himajin.dev>
2023-07-19 16:04:39 +0900

    Setup: Fix install scripts

--- scripts/setup_dotfiles.sh
+++ scripts/setup_dotfiles.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 
+mkdir -p $PWD/.config
+
+for dir in $PWD/.config/*
+do
+    ln -sfnv $dir $HOME/.config/
+done
+
+for file in $PWD/bin/*
+do
+    ln -sfnv $file $HOME/local/bin
+done
+
 for dotfile in .?*
 do
     if [ $dotfile != '..' ] && [ $dotfile != '.git' ] && [ $dotfile != '.gitignore' ] && [ $dotfile != '.gitmodules' ]
     then
         ln -sfnv "$PWD/$dotfile" $HOME
     fi
-done
-
-mkdir -p $PWD/.config
-
-for dir in $PWD/.config/*
-do
-    ln -sfnv $dir $HOME/.config
-done
-
-for file in $PWD/bin/*
-do
-    ln -sfnv $file $HOME/local/bin
 done
