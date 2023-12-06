82b407baf557d1704521a3cdd1046fdb0f805463
Kenta Okamoto <chocoby@himajin.dev>
2023-12-06 22:23:12 +0900

    Add nvim-cmp

--- nvim/init.lua
+++ nvim/init.lua
@@ -64,6 +64,43 @@ require('lazy').setup({
     end,
   },
   {
+    'hrsh7th/nvim-cmp',
+    event = 'InsertEnter',
+    dependencies = {
+      'hrsh7th/cmp-nvim-lsp',
+      'hrsh7th/cmp-buffer',
+      'hrsh7th/cmp-path',
+      'hrsh7th/cmp-vsnip',
+      'hrsh7th/cmp-cmdline',
+      'hrsh7th/vim-vsnip',
+    },
+    config = function()
+      local cmp = require("cmp")
+      cmp.setup({
+        snippet = {
+          expand = function(args)
+            vim.fn["vsnip#anonymous"](args.body)
+          end,
+        },
+        sources = {
+          { name = "nvim_lsp" },
+          { name = "buffer" },
+          { name = "path" },
+        },
+        mapping = cmp.mapping.preset.insert({
+          ["<C-p>"] = cmp.mapping.select_prev_item(),
+          ["<C-n>"] = cmp.mapping.select_next_item(),
+          ['<C-l>'] = cmp.mapping.complete(),
+          ['<C-e>'] = cmp.mapping.abort(),
+          ["<CR>"] = cmp.mapping.confirm { select = true },
+        }),
+        experimental = {
+          ghost_text = true,
+        },
+      })
+    end,
+  },
+  {
     "cappyzawa/trim.nvim",
     opts = {
       trim_on_write = true,
