10599027bc8824c08aa2138491dfd327eb753ac2
Kenta Okamoto <chocoby@himajin.dev>
2023-12-11 07:45:48 +0900

    Add keymap to search buffers

--- nvim/init.lua
+++ nvim/init.lua
@@ -238,6 +238,7 @@ vim.keymap.set('n', '<leader>sg', require('telescope.builtin').live_grep, { desc
 vim.keymap.set('n', '<leader>sG', ':LiveGrepGitRoot<cr>', { desc = '[S]earch by [G]rep on Git Root' })
 vim.keymap.set('n', '<leader>sd', require('telescope.builtin').diagnostics, { desc = '[S]earch [D]iagnostics' })
 vim.keymap.set('n', '<leader>sr', require('telescope.builtin').resume, { desc = '[S]earch [R]esume' })
+vim.keymap.set('n', '<leader>sb', require('telescope.builtin').buffers, { desc = '[S]earch [B]uffer' })
 
 
 -- nvim-tree
