f3d391e3e482eb7217398c1495f8f75509b719db
Kenta Okamoto <chocoby@himajin.dev>
2024-04-05 15:34:06 +0900

    Nvim: Fix replace CR character

--- nvim/init.lua
+++ nvim/init.lua
@@ -221,8 +221,8 @@ vim.g.clipboard = {
     ['*'] = "clip.exe",
   },
   paste = {
-    ['+'] = "powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace('`r', ''))",
-    ['*'] = "powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace('`r', ''))",
+    ['+'] = "powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace('\r', ''))",
+    ['*'] = "powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace('\r', ''))",
   },
   cache_enabled = 0,
 }
