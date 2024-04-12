61e2ec9792858c5c1f43352bb09f850b341bf0b6
Kenta Okamoto <chocoby@himajin.dev>
2024-04-12 10:54:12 +0900

    Nvim: Use default settings to access clipboard

--- nvim/init.lua
+++ nvim/init.lua
@@ -214,18 +214,6 @@ vim.opt.smartcase = true
 vim.opt.wrapscan = true
 
 -- Clipboard
-vim.g.clipboard = {
-  name = "WslClipboard",
-  copy = {
-    ['+'] = "clip.exe",
-    ['*'] = "clip.exe",
-  },
-  paste = {
-    ['+'] = "powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace('\r', ''))",
-    ['*'] = "powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace('\r', ''))",
-  },
-  cache_enabled = 0,
-}
 vim.opt.clipboard = "unnamedplus"
 
 -- [[ Keymap ]]
