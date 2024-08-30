9bb1eb3e7ebd94df9a61fd4eb271663e6df4163d
Kenta Okamoto <chocoby@himajin.dev>
2024-08-30 14:12:00 +0900

    Hammerspoon: Switch eikana

--- .hammerspoon/init.lua
+++ .hammerspoon/init.lua
@@ -12,3 +12,29 @@ end
 
 quitModal:bind('cmd', 'q', killApp)
 quitModal:bind('', 'escape', function() quitModal:exit() end)
+
+local simpleCmd = false
+local map = hs.keycodes.map
+local function eikanaEvent(event)
+  local c = event:getKeyCode()
+  local f = event:getFlags()
+  if event:getType() == hs.eventtap.event.types.keyDown then
+    if f['cmd'] then
+      simpleCmd = true
+    end
+  elseif event:getType() == hs.eventtap.event.types.flagsChanged then
+    if not f['cmd'] then
+      if simpleCmd == false then
+        if c == map['cmd'] then
+          hs.keycodes.setMethod('Alphanumeric (Google)')
+        elseif c == map['rightcmd'] then
+          hs.keycodes.setMethod('Hiragana (Google)')
+        end
+      end
+      simpleCmd = false
+    end
+  end
+end
+
+eikana = hs.eventtap.new({hs.eventtap.event.types.keyDown, hs.eventtap.event.types.flagsChanged}, eikanaEvent)
+eikana:start()
