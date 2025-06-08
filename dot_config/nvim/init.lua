17ecdc396a449e72b48068ff50e52c5ce8cc8df6
Kenta Okamoto <85665+chocoby@users.noreply.github.com>
2025-06-08 12:10:28 +0900

    Chezmoi: Install Neovim

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -478,42 +478,42 @@ local on_attach = function(_, bufnr)
 end
 
 -- mason
-require('mason').setup()
-require('mason-lspconfig').setup()
-local servers = {
-  solargraph = {
-    filetypes = { 'ruby' },
-  },
-  lua_ls = {
-    Lua = {
-      workspace = { checkThirdParty = false },
-      telemetry = { enable = false },
-      diagnostics = { globals = { "vim" } },
-    },
-  },
-}
-
--- nvim-cmp supports additional completion capabilities, so broadcast that to servers
-local capabilities = vim.lsp.protocol.make_client_capabilities()
-capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
-
--- Ensure the servers above are installed
-local mason_lspconfig = require 'mason-lspconfig'
-
-mason_lspconfig.setup {
-  ensure_installed = vim.tbl_keys(servers),
-}
-
-mason_lspconfig.setup_handlers {
-  function(server_name)
-    require('lspconfig')[server_name].setup {
-      capabilities = capabilities,
-      on_attach = on_attach,
-      settings = servers[server_name],
-      filetypes = (servers[server_name] or {}).filetypes,
-    }
-  end,
-}
+-- require('mason').setup()
+-- require('mason-lspconfig').setup()
+-- local servers = {
+--   solargraph = {
+--     filetypes = { 'ruby' },
+--   },
+--   lua_ls = {
+--     Lua = {
+--       workspace = { checkThirdParty = false },
+--       telemetry = { enable = false },
+--       diagnostics = { globals = { "vim" } },
+--     },
+--   },
+-- }
+--
+-- -- nvim-cmp supports additional completion capabilities, so broadcast that to servers
+-- local capabilities = vim.lsp.protocol.make_client_capabilities()
+-- capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
+--
+-- -- Ensure the servers above are installed
+-- local mason_lspconfig = require 'mason-lspconfig'
+--
+-- mason_lspconfig.setup {
+--   ensure_installed = vim.tbl_keys(servers),
+-- }
+--
+-- mason_lspconfig.setup_handlers {
+--   function(server_name)
+--     require('lspconfig')[server_name].setup {
+--       capabilities = capabilities,
+--       on_attach = on_attach,
+--       settings = servers[server_name],
+--       filetypes = (servers[server_name] or {}).filetypes,
+--     }
+--   end,
+-- }
 
 -- [[ Leap.nvim ]]
 require('leap').add_default_mappings()
