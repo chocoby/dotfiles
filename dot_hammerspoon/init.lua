5ace79340eeda6c6de3ab83c6b93aa91eab41741
Kenta Okamoto <85665+chocoby@users.noreply.github.com>
2026-04-12 12:07:36 +0900

    Hammerspoon: Remove eikana IME switching, use eikana app

--- dot_hammerspoon/init.lua
+++ dot_hammerspoon/init.lua
@@ -1,3 +1,4 @@
+-- Require double Cmd+Q to quit
 local quitModal = hs.hotkey.modal.new('cmd', 'q')
 
 function quitModal:entered()
@@ -12,29 +13,3 @@ end
 
 quitModal:bind('cmd', 'q', killApp)
 quitModal:bind('', 'escape', function() quitModal:exit() end)
-
-local simpleCmd = false
-local map = hs.keycodes.map
-local function eikanaEvent(event)
-  local c = event:getKeyCode()
-  local f = event:getFlags()
-  if event:getType() == hs.eventtap.event.types.keyDown then
-    if f['cmd'] then
-      simpleCmd = true
-    end
-  elseif event:getType() == hs.eventtap.event.types.flagsChanged then
-    if not f['cmd'] then
-      if simpleCmd == false then
-        if c == map['cmd'] then
-          hs.keycodes.setMethod('Alphanumeric (Google)')
-        elseif c == map['rightcmd'] then
-          hs.keycodes.setMethod('Hiragana (Google)')
-        end
-      end
-      simpleCmd = false
-    end
-  end
-end
-
-eikana = hs.eventtap.new({hs.eventtap.event.types.keyDown, hs.eventtap.event.types.flagsChanged}, eikanaEvent)
-eikana:start()
